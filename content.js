224938ea6e6fb83e3584ffdbe198d2143f8bc179
David Jacobowitz <dgj@lbl.gov>
2017-02-10 16:31:36 -0800

    add new mock-o-matic insult mode

--- content.js
+++ content.js
@@ -37,18 +37,62 @@ function get_randomize_time(mode) {
 }
 
 
-function choose_now(moniker_list,choice) {
-  var whichever = Math.floor(moniker_list.length * Math.random());
-  choice.last_chosen_item = whichever;
+// create a trump insult from raw ingredients
+function make_matic(action,recipe) {
+    var slots  = action.matic.slots;
+    var chunks = [];
+    var last_slot_name = '';
+    var last_elemidx = -1;
+    var tries = 0;
+    var max_tries = 10;
+    for (var i=0; i<recipe.length;i++) {
+        var slot_name = recipe[i];
+        var elemidx = Math.floor(slots[slot_name].length * Math.random());
+        while ((slot_name == last_slot_name) &&
+               (elemidx === last_elem_idx) &&
+               (tries < max_tries)
+              ) {
+            elemidx = Math.floor(slots[slot_name].length * Math.random());
+            tries += 1;
+        }
+        var chunkelem = slots[slot_name][elemidx];
+        if ((recipe.length > 2) && (i < (recipe.length-1))) {
+            // put commas between successive adjectives
+            chunkelem += ',';
+        }
+        chunks.push(chunkelem);
+        last_slot_name = slot_name;
+        last_elemidx = elemidx;
+    }
+    return chunks.join(' ');
+}
+
+// pick or generate a new insult immediately
+function choose_now(use_matic, moniker_list, action, choice) {
+  var item = null;
+
+  if ((use_matic !== "off") &&
+      action.hasOwnProperty('matic')
+     ) {
+      recipe = use_matic.toLowerCase().split(/_/);
+      log(recipe);
+      item = make_matic(action,recipe);
+  } else {
+      var item_idx = Math.floor(moniker_list.length * Math.random());
+      item     = moniker_list[item_idx % moniker_list.length];
+  }
+  choice.last_chosen_item = item;
   choice.last_chosen_time = (new Date()).getTime();
 }
 
-function choose(rand_mode,moniker_list,choice) {
+// return the next insult -- may be the same as the last
+// insult or a new one depending on time and user mode
+function choose(rand_mode, use_matic, moniker_list, action, choice) {
   var wait = get_randomize_time(rand_mode);
   var now = (new Date()).getTime();
-  if (!choice.hasOwnProperty('last_chosen_time') || 
+  if (!choice.hasOwnProperty('last_chosen_time') ||
       ((now - choice.last_chosen_time) >= wait)) {
-    choose_now(moniker_list,choice);
+    choose_now(use_matic, moniker_list, action,choice);
   }
   return choice;
 }
@@ -87,13 +131,24 @@ function find_match_nonmatch_chunks(text,re) {
 }
 
 
-function make_replacement_elems_array(action_name,action,rand_mode,moniker_list,brackets,broken_texts,orig_node,choice) {
+function make_replacement_elems_array(args) {
+
+  var action_name  = useIfElse(args, 'action_name', '__error_missing_action_name');
+  var action       = args.action;
+  var rand_mode    = useIfElse(args, 'rand_mode',   'always');
+  var use_matic    = useIfElse(args, 'use_matic',   'off');
+  var moniker_list = useIfElse(args, 'monikers',    []);
+  var brackets     = useIfElse(args, 'brackets',    [ '', '' ]);
+  var broken_texts = args.broken_texts;
+  var orig_node    = args.node;
+  var choice       = args.choice;
+
   var repl_array = [];
   for (var k=0;k<broken_texts.length;k++) {
     chunk = broken_texts[k];
     if (chunk.match) {
-      choose(rand_mode, moniker_list, choice);
-      var replacement = moniker_list[choice.last_chosen_item % moniker_list.length];
+      choose(rand_mode, use_matic, moniker_list, action, choice);
+      var replacement = choice.last_chosen_item;
       replacement = brackets[0] + replacement + brackets[1];
 
       var unode = document.createElement('span');
@@ -134,25 +189,30 @@ function switchem() {
   if (count) {
     log('currTimeout:' + currTimeout);
     setTimeout(switchem,currTimeout);
-    if (currTimeout < maxTimeout) currTimeout *= current_config.run_info.timeMultiplier;
+    if (currTimeout < maxTimeout) {
+          currTimeout *= current_config.run_info.timeMultiplier;
+    }
   }
 }
 
 function getRunnableActions(config_actions,items) {
     // generate a shortened list of actions that the user has enabled
-    var actions_to_run = Object.keys(current_config.actions);
-    if (items.hasOwnProperty('enabled_actions')) {
-      temp_actions_to_run = Object.keys(items.enabled_actions);
-      actions_to_run = [];
-      for (n=0;n<temp_actions_to_run.length; n++) {
-        action_name = temp_actions_to_run[n];
-        if (current_config.actions.hasOwnProperty(action_name) &&
-            (items.enabled_actions[action_name])) {
-          actions_to_run.push(action_name);
+
+    var actions_to_run = [];
+    var action_names = Object.keys(current_config.actions);
+    for (var i=0; i< action_names.length; i++) {
+        var action_name = action_names[i];
+        var enable_this = true;
+        if (items.hasOwnProperty('enabled_actions') &&
+            items.enabled_actions.hasOwnProperty(action_name)) {
+            enable_this = items.enabled_actions[action_name];
+        } else if (current_config.actions[action_name].hasOwnProperty('default_enabled')) {
+            enable_this = Boolean(config_actions[action_name].default_enabled);
         }
-      }
+        if (enable_this) actions_to_run.push(action_name);
     }
     return actions_to_run;
+
 }
 
 // this routine starts with an element and tries to work up
@@ -182,13 +242,45 @@ function findParentLinkMatch(elem, re) {
     return false;
 }
 
+function makeImageReplacementDiv(img, action) {
+    var border = 2;
+    var nd = document.createElement('div');
+    var repl = document.createElement('span');
+    repl.textContent = 'National disgrace removed.';
+    var backsrc = null;
+    if (action.hasOwnProperty('image_replacement')) {
+        var idx = Math.floor(Math.random()*action.image_replacement.html.length);
+        repl.textContent = action.image_replacement.html[idx];
+        border = action.image_replacement.border;
+        if (action.image_replacement.hasOwnProperty('background')) {
+            idx = Math.floor(Math.random()*action.image_replacement.background.length);
+            backsrc = action.image_replacement.background[idx];
+        }
+    }
+    if (backsrc) {
+        nd.style.backgroundImage = "url('" + backsrc + "')";
+        nd.style['background-repeat'] = 'repeat';
+    }
+    removeChildrenReplaceWith(nd,[repl]);
+    var dw = img.clientWidth - 2 * border;
+    var dh = img.clientHeight - 2 * border;
+    nd.style['text-align'] = 'center';
+    nd.style['vertical-align'] = 'middle';
+    nd.style.display = 'table-cell';
+    nd.style.border = border.toString() + 'px solid';
+    nd.style.width = dw.toString() + 'px';
+    nd.style['max-width'] = dw.toString() + 'px';
+    nd.style.height= dh.toString() + 'px';
+    nd.style['max-height'] = dh.toString() + 'px';
+    return nd;
+}
 
 function switch_imgs() {
   log('switch_imgs()');
   chrome.storage.local.get(['kittenize','enabled_actions'],function(items) {
     var action_count = 0;
 
-    var mode = items.hasOwnProperty('kittenize') ? items.kittenize : 'off';
+    var mode = useIfElse(items, 'kittenize', 'off');
 
     // this is for backward compatibility with boolean version that
     // may be stored in local store.
@@ -238,40 +330,12 @@ function switch_imgs() {
               false) {
             var replsrc;
             if (mode == 'div') {
-                var border = 2;
-                var nd = document.createElement('div');
-                var repl = document.createElement('span');
-                repl.textContent = 'National disgrace removed.';
-                var backsrc = null;
-                if (action.hasOwnProperty('image_replacement')) {
-                    var idx = Math.floor(Math.random()*action.image_replacement.html.length);
-                    repl.textContent = action.image_replacement.html[idx];
-                    border = action.image_replacement.border;
-                    if (action.image_replacement.hasOwnProperty('background')) {
-                        idx = Math.floor(Math.random()*action.image_replacement.background.length);
-                        backsrc = action.image_replacement.background[idx];
-                    }
-                }
-                if (backsrc) {
-                    nd.style.backgroundImage = "url('" + backsrc + "')";
-                    nd.style['background-repeat'] = 'repeat';
-                }
-                removeChildrenReplaceWith(nd,[repl]);
-                var dw = img.clientWidth - 2 * border;
-                var dh = img.clientHeight - 2 * border;
-                nd.style['text-align'] = 'center';
-                nd.style['vertical-align'] = 'middle';
-                nd.style.display = 'table-cell';
-                nd.style.border = border.toString() + 'px solid';
-                nd.style.width = dw.toString() + 'px';
-                nd.style['max-width'] = dw.toString() + 'px';
-                nd.style.height= dh.toString() + 'px';
-                nd.style['max-height'] = dh.toString() + 'px';
+                var nd = makeImageReplacementDiv(img, action);
                 img.parentNode.replaceChild(nd,img);
             } else if ((mode == 'kittens') ||
                        (mode == 'more_kittens')) {
                 replsrc = 'https://placekitten.com/' +
-                          img.clientWidth.toString() + '/' + 
+                          img.clientWidth.toString() + '/' +
                           img.clientHeight.toString();
                 if (img.src !== replsrc) img.src = replsrc;
             } else {
@@ -287,6 +351,37 @@ function switch_imgs() {
   });
 }
 
+function filterListByWordCount(list, max_words) {
+    var olist = [];
+    for (var p=0; p<list.length; p++) {
+        var item = list[p];
+        var word_count = item.split(/[\s\-]/).length;
+        if ((max_words === 0) || (word_count <= max_words)) {
+            olist.push(item);
+        }
+    }
+    return olist;
+}
+
+function useIfElse(dict, name, deflt) {
+    return dict.hasOwnProperty(name) ? dict[name] : deflt;
+}
+
+function determineBrackets(mode, action) {
+    var brackets = ['',''];
+    if (action.hasOwnProperty('scarequote') && action.scarequote) {
+        brackets = [ '\u201c', '\u201d' ];
+    }
+    if (action.hasOwnProperty('bracket') && (action.bracket.length >= 2)) {
+        brackets = [ action.bracket[0], action.bracket[1] ];
+    }
+    switch (mode) {
+        case 'curly'  : brackets = [ '\u201c', '\u201d' ]; break;
+        case 'square' : brackets = [ '[', ']' ]; break;
+    }
+    return brackets;
+}
+
 function switch_text() {
     log('switchem START, count:' + count);
 
@@ -299,13 +394,15 @@ function switch_text() {
     var action_name;
     var n;
 
-    chrome.storage.local.get(['stored_choices','enabled_actions','brevity','brackets','rand_mode'],function(items) {
+    chrome.storage.local.get(
+        ['stored_choices','enabled_actions','brevity','brackets',
+         'rand_mode','use_matic',],
+        function(items) {
       var action_count = 0;
-      var stored_choices = {};
-      if (items.hasOwnProperty('stored_choices')) stored_choices = items.stored_choices;
-      log(stored_choices);
+      var stored_choices = useIfElse(items, 'stored_choices', {});
 
       var actions_to_run = getRunnableActions(current_config.actions,items);
+      log(actions_to_run);
 
       for (var n=0; n<actions_to_run.length; n++) {
         action_name = actions_to_run[n];
@@ -317,42 +414,16 @@ function switch_text() {
           return;
         }
 
+        var brevity   = useIfElse(items, 'brevity', '0');
+        var use_matic = useIfElse(items, 'use_matic', 'off');
+        var rand_mode = useIfElse(items, 'rand_mode', 'always');
+
         // create a sublist of monikers that meet the brevity criteria
-        var monikers_to_use = action.monikers;
-        if (items.hasOwnProperty('brevity')) {
-            monikers_to_use = [];
-            var max_l = parseInt(items.brevity);
-            for (var p=0; p<action.monikers.length; p++) {
-                var moniker = action.monikers[p];
-                var word_count = moniker.split(/[\s\-]/).length;
-                if ((max_l === 0) || (word_count <= max_l)) {
-                    monikers_to_use.push(moniker);
-                }
-            }
-        }
+        var max_wordcount = parseInt(brevity);
+        var monikers_to_use = filterListByWordCount(action.monikers, max_wordcount);
 
-        var rand_mode = 'always';
-        if (items.hasOwnProperty('rand_mode')) {
-            rand_mode = items.rand_mode;
-        }
-
-        // Determine quoting. I am gradually moving away from 
-        // the config file for this, and letting the UI take
-        // precendence. Eventually, I will not include the 
-        // config file settings at all.
-        var brackets = ['',''];
-        if (action.hasOwnProperty('scarequote') && action.scarequote) {
-          brackets = [ '\u201c', '\u201d' ];
-        }
-        if (action.hasOwnProperty('bracket') && (action.bracket.length >= 2)) {
-          brackets = [ action.bracket[0], action.bracket[1] ];
-        }
-        if (items.hasOwnProperty('brackets')) {
-            var mode = items.brackets;
-            if (mode === 'curly') brackets = [ '\u201c', '\u201d' ];
-            if (mode === 'square') brackets = [ '[', ']' ];
-        }
-
+        var bracket_mode = useIfElse(items,'brackets','off');
+        var brackets = determineBrackets(bracket_mode, action);
 
         var search_regex = new RegExp(action.find_regex[0],
                                       action.find_regex[1]);
@@ -360,7 +431,7 @@ function switch_text() {
 
         if (!stored_choices.hasOwnProperty(action_name)) {
           stored_choices[action_name] = {};
-          choose_now(monikers_to_use,stored_choices[action_name]);
+          choose_now(use_matic, monikers_to_use, action, stored_choices[action_name]);
         }
 
         for (var i = 0; i < elements.length; i++) {
@@ -372,15 +443,18 @@ function switch_text() {
               broken_texts = find_match_nonmatch_chunks(text,search_regex);
 
               if (broken_texts.length) {
-                repl_array = make_replacement_elems_array(
-                        action_name,
-                        action,
-                        rand_mode,
-                        monikers_to_use,
-                        brackets,
-                        broken_texts,
-                        node,
-                        stored_choices[action_name]);
+                repl_array = make_replacement_elems_array({
+                    action_name: action_name,
+                    action: action,
+                    rand_mode: rand_mode,
+                    broken_texts: broken_texts,
+                    use_matic: use_matic,
+                    monikers: monikers_to_use,
+                    brackets: brackets,
+                    node: node,
+                    choice: stored_choices[action_name]
+                });
+
                 replace_elem_with_array_of_elems(node,repl_array);
               }
             }
